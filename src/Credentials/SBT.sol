34140b52c00e696a33c280a4e414c35cd81e2c1b
1M4nt0 <andrea.antonutti@gmail.com>
2023-05-01 15:00:44 +0200

    public baseURIextended

--- src/Credentials/SBT.sol
+++ src/Credentials/SBT.sol
@@ -52,11 +52,12 @@ contract SoulboundNFT is ERC721, ERC721Enumerable, ERC721URIStorage, IERC5192 {
         _baseURIextended = baseURI_;
     }
 
-    // Required Overrides
-
-    function _baseURI() internal view virtual override returns (string memory) {
+    function baseURI() public view returns (string memory) {
         return _baseURIextended;
     }
+
+
+    // Required Overrides
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
