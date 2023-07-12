24e4b31a7f719e5caa1c1fc1d6c808f7e95e4495
1M4nt0 <andrea.antonutti@gmail.com>
2023-07-12 15:40:02 +0200

    check

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,16 +4,13 @@ pragma solidity ^0.8.9;
 import "forge-std/Script.sol";
 import "src/Credentials/Credentials.sol";
 import "src/Karma/KarmaAccessControluint64.sol";
+import "src/Credentials/Factory.sol";
 
 contract LocalDeploy is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-
-        CredentialsBurnable creds =
-            new CredentialsBurnable(msg.sender, "Credentials", "CREDS", "https://creds.com/", 100);
-        new KarmaAccessControluint64(creds, 0, 0);
-
+        new CredentialsFactory();
         vm.stopBroadcast();
     }
 }
